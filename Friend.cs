721b7fd0b2035dd5f0905366b4028b877f0b7ca7
DenisOST <den.ostroukhov.03@mail.ru>
2025-05-06 00:21:35 +0700

    Create class "Friend". New method - return greeting string with name.

---
+++ Friend.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleFriend
+{
+    public class Friend
+    {
+        public static string GetGreeting(string name)
+        {
+            return $"Привет, {name}! Как дела?";
+        }
+    }
+}
